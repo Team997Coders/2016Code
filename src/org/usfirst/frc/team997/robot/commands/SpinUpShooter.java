00824d2daf976cccb570050bb1dd4eed53a989f1
Floyd Moore <floyd.moore@gmail.com>
2016-02-08 10:02:42 -0800

    Working on commands and the shooter subsystem.
    Testing a Bang-Bang controller in the shooter.

--- src/org/usfirst/frc/team997/robot/commands/SpinUpShooter.java
+++ src/org/usfirst/frc/team997/robot/commands/SpinUpShooter.java
@@ -42,11 +42,12 @@ public class SpinUpShooter extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.shooter.spinup(0.8);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
