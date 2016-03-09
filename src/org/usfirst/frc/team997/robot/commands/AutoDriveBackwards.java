54a3d9bf3e117ce5702523a78a386c440bf3a9d6
czipperz <czipperz@gmail.com>
2016-03-08 16:43:59 -0800

    Fix formatting in `AutoDriveBackwards`

--- src/org/usfirst/frc/team997/robot/commands/AutoDriveBackwards.java
+++ src/org/usfirst/frc/team997/robot/commands/AutoDriveBackwards.java
@@ -6,8 +6,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class AutoDriveBackwards extends CommandGroup {
-    
-    public  AutoDriveBackwards() {
+    public AutoDriveBackwards() {
         addParallel(ShooterToAngle.low);
         addSequential(GathererToAngle.low);
         addSequential(new DriveToSetpointBackwards(-100));
