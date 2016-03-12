eaedbeb50a35256d0e4e09dec5312d3347dd6092
czipperz <czipperz@gmail.com>
2016-03-12 07:50:12 -0800

    Log status of all autonomous routines

--- src/org/usfirst/frc/team997/robot/commands/AutoCheval.java
+++ src/org/usfirst/frc/team997/robot/commands/AutoCheval.java
@@ -7,10 +7,14 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class AutoCheval extends CommandGroup {
     public AutoCheval() {
+    	addSequential(new LogOnInitBoolean("AutoCheval running", true));
+
     	addParallel(ShooterToAngle.middleLow());
     	addParallel(GathererToAngle.mid());
     	addSequential(new DriveToSetpoint(24)); //2ft
     	addParallel(GathererToAngle.low());
     	addSequential(new DriveToSetpoint(60));
+
+    	addSequential(new LogOnInitBoolean("AutoCheval running", false));
     }
 }
--- src/org/usfirst/frc/team997/robot/commands/AutoDriveForward.java
+++ src/org/usfirst/frc/team997/robot/commands/AutoDriveForward.java
@@ -7,9 +7,13 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class AutoDriveForward extends CommandGroup {
     public AutoDriveForward() {
+    	addSequential(new LogOnInitBoolean("AutoDriveForward running", true));
+
     	// needs in parallel as they don't finish for some reason
     	addParallel(ShooterToAngle.middleLow());
     	addParallel(GathererToAngle.mid());
     	addSequential(new DriveToSetpoint(100));
+
+    	addSequential(new LogOnInitBoolean("AutoDriveForward running", false));
     }
 }
