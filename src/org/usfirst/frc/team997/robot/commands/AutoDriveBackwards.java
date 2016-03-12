b4736377fa33bbce981f3d2ca1a12888cc6eb6e3
czipperz <czipperz@gmail.com>
2016-03-11 16:27:17 -0800

    Log when `AutoDriveBackwards` is running

--- src/org/usfirst/frc/team997/robot/commands/AutoDriveBackwards.java
+++ src/org/usfirst/frc/team997/robot/commands/AutoDriveBackwards.java
@@ -7,9 +7,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class AutoDriveBackwards extends CommandGroup {
     public AutoDriveBackwards() {
+        addSequential(new LogOnInitBoolean("AutoDriveBackwards running", true));
+
         //Use this auto routine for the low bar.
         addParallel(ShooterToAngle.high()); //actually makes shooter low
         addParallel(GathererToAngle.low());
         addSequential(new DriveToSetpointBackwards(-100));
+        addSequential(new LogOnInitBoolean("AutoDriveBackwards running", false));
     }
 }
