ab41007f598a13866bf8c2a96d4b8148511f7ab1
Czipperz <czipperz@gmail.com>
2016-09-27 16:20:41 -0700

    Value changes after motor adjustments, controller changes

--- src/org/usfirst/frc/team997/robot/RobotMap.java
+++ src/org/usfirst/frc/team997/robot/RobotMap.java
@@ -23,9 +23,9 @@ public class RobotMap {
 	bannerEncoderBallPort              = 5, // shooter
 
 	// Analog
-	shooterPivotAnglePort              = 0, // shooter angle
-	shooterBallSensor                  = 1, // infrared ball sensor
-	gathererArmAnglePort               = 2, // shooter
+	shooterPivotAnglePort              = 0,
+	shooterBallSensor                  = 2,
+	gathererArmAnglePort               = 1,
 
 	// Spike/Relay
 	circleLightPort                    = 0, // misc
@@ -58,9 +58,9 @@ public class RobotMap {
 		shooterPivotMiddleHigh         = 0.57, // High Medium shooter position
 
 		collectArmPostion              = 0.675, // Where should the arm be to collect balls
-		gathererArmBeforeHitRobot      = 0.193, // Highest gatherer arm position (vertical) CHANGED FROM .134 after motor adjustment
-		gathererArmBeforeHitGround     = 0.774, // Lowest gatherer arm position (flat)
-		gathererArmMid                 = 0.453, //mid gatherer arm position (avg between high and low)
+		gathererArmBeforeHitRobot      = 0.25, // Highest gatherer arm position (vertical) CHANGED FROM .134 after motor adjustment
+		gathererArmBeforeHitGround     = 0.85, // Lowest gatherer arm position (flat)
+		gathererArmMid                 = 0.453, //mid gatherer arm position (avg between highh and low)
 
 		kickerRightMin                 = 1.0, // NEED TO CHANGE distance out when kicking
 		kickerRightMax                 = 0.0, // distance out when retracting
