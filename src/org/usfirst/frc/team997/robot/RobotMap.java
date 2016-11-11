38e9ccdbd50447c1b1c3a4fc6ea0640600c30555
Czipperz <czipperz@gmail.com>
2016-11-11 15:05:34 -0800

    Changed values in RobotMap

--- src/org/usfirst/frc/team997/robot/RobotMap.java
+++ src/org/usfirst/frc/team997/robot/RobotMap.java
@@ -52,15 +52,17 @@ public class RobotMap {
 
 	public static class Voltages {
 		public static final double
-		shooterPivotRobot              = 0.51, // Highest shooter position     
-		shooterPivotGround             = 0.675, // Lowest shooter position     
-		shooterPivotMiddleLow          = 0.63, // Low Medium shooter position       
-		shooterPivotMiddleHigh         = 0.57, // High Medium shooter position
+		shooterPivotRobot              = 0.51, // 0.68, // Lowest shooter position
+		shooterPivotGround             = 0.675, // 0.83, // Highest shooter position
+		shooterPivotMiddleLow          = 0.63, // 0.77, // Low Medium shooter position
+		shooterPivotMiddleHigh         = 0.57, // 0.72, // High Medium shooter position
+		shooterShiftInc                = 0.01, // Increment for manual override
 
 		collectArmPostion              = 0.675, // Where should the arm be to collect balls
-		gathererArmBeforeHitRobot      = 0.25, // Highest gatherer arm position (vertical) CHANGED FROM .134 after motor adjustment
+		gathererArmBeforeHitRobot      = 0.25, // Highest gatherer arm position (vertical)
 		gathererArmBeforeHitGround     = 0.85, // Lowest gatherer arm position (flat)
-		gathererArmMid                 = 0.453, //mid gatherer arm position (avg between highh and low)
+		gathererArmMid                 = 0.453, //mid gatherer arm position (avg between high and low)
+		gathererShiftInc               = 0.05, // gathers shift increment
 
 		kickerRightMin                 = 1.0, // NEED TO CHANGE distance out when kicking
 		kickerRightMax                 = 0.0, // distance out when retracting
